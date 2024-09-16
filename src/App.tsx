6d599dc1206536ead5a16f522876d0dc3ac6763a
Marcelo Salloum <marcelo@stellar.org>
2024-09-16 15:55:27 -0700

    Update config.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import { Layout } from "@stellar/design-system";
 import { Outlet } from "react-router-dom";
 import { QueryProvider } from "@/query/QueryProvider";
-
-const PROJECT_ID = "project-template";
-const PROJECT_TITLE = "Project";
+import { PROJECT_ID, PROJECT_TITLE } from "@/config/settings";
 
 export const App = () => {
   return (
     <QueryProvider>
       <Layout.Header
-        projectId={PROJECT_ID}
-        projectTitle={PROJECT_TITLE}
+        projectId={PROJECT_ID || "meridian-2024-smart-wallet"}
+        projectTitle={PROJECT_TITLE || "Smart Wallet"}
         hasThemeSwitch
       />
       <Layout.Content>
