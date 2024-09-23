86ca6058e800c22c3dbfda06749dd3590ee8208a
Marcelo Salloum <marcelo@stellar.org>
2024-09-23 11:01:14 -0700

    Add SEP-10c request/response types.

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,3 +15,29 @@ export type SorobanEntryAddress = {
   type: xdr.ScAddressType; // xdr.ScAddressType.scAddressTypeAccount() | xdr.ScAddressType.scAddressTypeContract();
   scAddress: xdr.ScAddress;
 };
+
+export type GetSEP10cChallengeRequest = {
+  address: string;
+  memo?: string;
+  home_domain?: string;
+  client_domain?: string;
+};
+
+export type GetSEP10cChallengeResponse = {
+  // authorization_entry is a XDR string representation of xdr.SorobanAuthorizationEntry
+  authorization_entry: string;
+  // server_signature is a XDR string representation of xdr.SorobanSignature
+  server_signature: string;
+};
+
+export type PostSEP10cChallengeRequest = {
+  // authorization_entry is a XDR string representation of xdr.SorobanAuthorizationEntry
+  authorization_entry: string;
+  server_signature: string;
+  credentials: string[];
+};
+
+export type PostSEP10cChallengeResponse = {
+  // token is a jwt token
+  token: string;
+};
