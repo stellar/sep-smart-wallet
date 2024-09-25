1b241bc80503bc9b9ad8eddbbc9d05136933e8aa
Marcelo Salloum <marcelo@stellar.org>
2024-09-25 12:24:42 -0700

    Use localStorage to persist TokenStore and ContractSignerStore

--- src/store/useContractSignerStore.ts
+++ src/store/useContractSignerStore.ts
@@ -1,12 +1,23 @@
-import { ContractSigner } from "@/types/types";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+import { ContractSigner } from "@/types/types";
 
 interface ContractSignerStore {
   contractSigner: ContractSigner | null;
   setContractSigner: (signer: ContractSigner | null) => void;
 }
 
-export const useContractSignerStore = create<ContractSignerStore>((set) => ({
-  contractSigner: null,
-  setContractSigner: (signer) => set({ contractSigner: signer }),
-}));
+// Create the Zustand store with TypeScript types
+export const useContractSignerStore = create<ContractSignerStore>()(
+  persist(
+    (set) => ({
+      contractSigner: null,
+      setContractSigner: (signer: ContractSigner | null) => set({ contractSigner: signer }),
+    }),
+    {
+      name: "ssw:contractSigner", // Unique name for the localStorage key
+      getStorage: () => localStorage, // Choose storage type (localStorage here)
+    },
+  ),
+);
